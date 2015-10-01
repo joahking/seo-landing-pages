3eb63ddcbbfc7d665971f1a7f578e15d3cc77711
Joaquin Rivera Padron <joahking@gmail.com>
2015-10-01 14:07:22 +0200

    Controller helpers done, #current_landing_page created

--- lib/seo_landing_pages.rb
+++ lib/seo_landing_pages.rb
@@ -2,5 +2,9 @@ require 'seo_landing_pages/version'
 
 module SeoLandingPages
   autoload :Model, 'seo_landing_pages/model'
+
+  module Controllers
+    autoload :Helpers, 'seo_landing_pages/controllers/helpers'
+  end
 end
 
---
+++ lib/seo_landing_pages/controllers/helpers.rb
@@ -0,0 +1,40 @@
+# Public: helper methods for ApplicationController.
+#
+# Include this module in your controllers to gain access to the landing page
+# in your views.
+#
+module SeoLandingPages
+  module Controllers
+    module Helpers
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :current_landing_page
+      end
+
+      # Public: returns the current landing page.
+      # The landing page is matched by the request path, check the method
+      # seo_request_path for details on how to change the matching.
+      def current_landing_page
+        @current_landing_page ||= SeoLandingPages::Model.where(slug: seo_request_path).first
+      end
+
+      protected
+
+      # Internal: the request path is used to match landing pages.
+      #
+      # Override this method in your controllers to change the way the landing
+      # pages are matched.
+      #
+      # Example: the request path is '/blog?search=XYZ', to handle all blog pages
+      # as the same landing page (e.g. '/blog') then change
+      # this method in your blogs_controller to remove everything after the '?'
+      #
+      def seo_request_path
+        request.path
+      end
+
+    end
+  end
+end
+
